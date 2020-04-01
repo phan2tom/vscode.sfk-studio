a3082041e4776c2cde0c8f2c5e0732333cce2831
Tom PHAN <phan2tom@gmail.com>
2020-04-01 17:30:56 +0200

    GoToDefinition: Use RegExp for TS files

--- src/commands/goToDefinitionTypeScript.ts
+++ src/commands/goToDefinitionTypeScript.ts
@@ -6,19 +6,50 @@ export default class GoToDefinitionTypeScript {
 	static async execute() {
 		if (vscode.window.activeTextEditor !== undefined) {
 			const activeTextEditor = vscode.window.activeTextEditor;
-			let symbols = await vscode.commands.executeCommand<vscode.DocumentSymbol[]>('vscode.executeDocumentSymbolProvider', activeTextEditor.document.uri);
-			if (symbols) {
-					new GoToDefinitionTypeScript().goToDefinition(activeTextEditor, symbols);
-			}
+			new GoToDefinitionTypeScript().goToDefinition(activeTextEditor);
 		}
 	}
 
-	protected goToDefinition(activeTextEditor: vscode.TextEditor, symbols: vscode.DocumentSymbol[] | undefined) {
-		let currentPosition = activeTextEditor.selection.start;
-		let wordRange = activeTextEditor.document.getWordRangeAtPosition(currentPosition);
+	protected goToDefinition(activeTextEditor: vscode.TextEditor) {
+		let line = activeTextEditor.document.lineAt(activeTextEditor.selection.active.line);
 
-		if (!symbols || !wordRange) {
+		let m = line.text.match(/\$(?<service>\w+)\s*\.\s*(?<method>\w+)\s*\(\s*['"](?<firstArg>[^"']+)['"]/);
+		if (!m || !m.groups) {
 			return;
+		}
+
+		let target = this.getTarget(m.groups['service'], m.groups['method'], m.groups['firstArg']);
+		vscode.window.showInformationMessage(target);
+	}
+
+	private getTarget(service: string, method: string, firstArg: string) {
+		switch (service) {
+			case 'component': return this.getTargetComponent(method, firstArg);
+			case 'database': return this.getTargetDatabase(method, firstArg);
+			case 'library': return this.getTargetLibrary(method, firstArg);
+			default: return '';
+		}
+	}
+
+	private getTargetComponent(method: string, firstArg: string) {
+		switch (method) {
+			case 'getById': return `Component ${firstArg}`;
+			default: return '';
+		}
+	}
+	private getTargetDatabase(method: string, firstArg: string) {
+		switch (method) {
+			case 'insertAsync': return `DataSource ${firstArg}`;
+			case 'selectAsync': return `DataSource ${firstArg}`;
+			case 'updateAsync': return `DataSource ${firstArg}`;
+			case 'deleteAsync': return `DataSource ${firstArg}`;
+			default: return '';
+		}
+	}
+	private getTargetLibrary(method: string, firstArg: string) {
+		switch (method) {
+			case 'getLibraryAsync': return `ScriptLibrary ${firstArg}`;
+			default: return '';
 		}
 	}
 }
