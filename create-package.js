ccb02c6f9679636e2b8d7e8ddcc5fcb3ff65a441
Tom PHAN <62723514+phan2tom@users.noreply.github.com>
2020-04-05 11:49:10 +0200

    vscode: Adapt create-package.js to webpack

--- create-package.js
+++ create-package.js
@@ -1,28 +1,23 @@
 const child_process = require('child_process');
 
 async function pack() {
-  await exec('npm install -P typescript@3.5.3');
-  await exec('vsce package');
-  await exec('npm install -D typescript@3.8.3');
+  await exec('node_modules\\.bin\\webpack', ['--mode', 'production']);
+  await exec('npm', ['i', '-P', 'typescript@3.5.3']);
+  await exec('vsce', ['package']);
+  await exec('npm', ['i', '-D', 'typescript@3.8.3']);
 }
 pack();
 
-function exec(cmd) {
+function exec(cmd, args) {
   return new Promise((resolve, reject) => {
-    child_process.exec(cmd, (error, stdout, stderr) => {
-      let out = stdout;
-      if (error) {
-        out += `\n${stderr}`;
+    let proc = child_process.spawn(cmd, args, { stdio: 'inherit', shell: true });
+    proc.on('close', code => {
+      if (code !== 0) {
+        console.log(`Process exited with code ${code}`);
+        reject();
+        process.exit(code);
       }
-      if (out) {
-        console.log(out);
-      }
-      if (error) {
-        reject(error);
-      }
-      else {
-        resolve();
-      }
+      resolve();
     });
   });
 }
